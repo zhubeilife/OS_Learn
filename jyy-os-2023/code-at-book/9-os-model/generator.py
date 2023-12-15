bbc08c599fd1239330655edd29fdd855d845d7ae
BryanZhang <bryan.zhang@hairobotics.com>
2023-12-15 11:53:54 +0800

    update

--- jyy-os-2023/code-at-book/9-os-model/generator.py
+++ jyy-os-2023/code-at-book/9-os-model/generator.py
@@ -1,5 +1,11 @@
 def numbers():
     i = 0
     while True:
-        ret = yield f'{i:b}'
+        ret = yield f'{i:b}'   # “封存” 状态机状态
         i += ret
+
+
+# usage
+# n = numbers()  # 封存状态机初始状态
+# n.send(None)  # 恢复封存的状态
+# n.send(0)  # 恢复封存的状态 (并传入返回值)
