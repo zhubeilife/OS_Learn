7f867ce4f4d1689b8e2dd559ce5e420ec94d3b32
zhubeilife <zhubeilife@gmail.com>
2023-12-21 10:10:04 +0800

    M1-Step-1

--- jyy-os-2023/exercise/M1/pstree/getopt.c
+++ jyy-os-2023/exercise/M1/pstree/getopt.c
@@ -1,46 +1,46 @@
-
-// The following trivial example program uses getopt() to handle two program options: -n,  with
-// no associated value; and -t val, which expects an associated value.
-
 #include <unistd.h>
 #include <stdlib.h>
+#include <getopt.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int
+main(int argc, char** argv)
 {
-    int flags, opt;
-    int nsecs, tfnd;
+    while (1)
+    {
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"show-pids", no_argument, 0, 'p'},
+            {"numeric-sort", no_argument, 0, 'n'},
+            {"version", no_argument, 0, 'V'}
+        };
 
-    nsecs = 0;
-    tfnd = 0;
-    flags = 0;
-    while ((opt = getopt(argc, argv, "nt:")) != -1) {
-        switch (opt) {
+        int c = getopt_long(argc, argv, "pnV", long_options, &option_index);
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+        case 'p':
+            printf("get p \n");
+            break;
+
         case 'n':
-            flags = 1;
+            printf("get n \n");
             break;
-        case 't':
-            nsecs = atoi(optarg);
-            tfnd = 1;
+
+        case 'V':
+            printf("get V \n");
             break;
-        default: /* '?' */
-            fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n",
-                    argv[0]);
-            exit(EXIT_FAILURE);
+
+        case '?':
+            printf("Usage: -V --version -p --show-pids -n --numeric-sort\n");
+            break;
+
+        default:
+            fprintf(stderr, "?? getopt returned character code 0%o ??\n", c);
         }
     }
-
-    printf("flags=%d; tfnd=%d; nsecs=%d; optind=%d\n",
-            flags, tfnd, nsecs, optind);
-
-    if (optind >= argc) {
-        fprintf(stderr, "Expected argument after options\n");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("name argument = %s\n", argv[optind]);
-
-    /* Other code omitted */
 
     exit(EXIT_SUCCESS);
 }
--- jyy-os-2023/exercise/M1/pstree/pstree.c
+++ jyy-os-2023/exercise/M1/pstree/pstree.c
@@ -1,11 +1,51 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
-  }
-  assert(!argv[argc]);
-  return 0;
+char* PSTREE_VERSION = "0.0.1";
+
+int main(int argc, char* argv[])
+{
+    // Step-1 parse the params
+    bool show_pids = false;
+    bool numeric_sort = false;
+    while (1)
+    {
+        int option_index = 0;
+        static struct option long_options[] = {
+            {"show-pids", no_argument, 0, 'p'},
+            {"numeric-sort", no_argument, 0, 'n'},
+            {"version", no_argument, 0, 'V'}
+        };
+
+        int c = getopt_long(argc, argv, "pnV", long_options, &option_index);
+        if (c == -1)
+            break;
+
+        switch (c)
+        {
+        case 'p':
+            show_pids = true;
+            break;
+        case 'n':
+            numeric_sort = true;
+            break;
+        case 'V':
+            printf("Current version is %s.\n", PSTREE_VERSION);
+            exit(EXIT_SUCCESS);
+            break;
+        case '?':
+            printf("Usage: -V --version -p --show-pids -n --numeric-sort\n");
+            break;
+        default:
+            fprintf(stderr, "?? getopt returned character code 0%o ??\n", c);
+        }
+    }
+    // show debug info
+    printf("debug: show-pids:%d numeric-sort:%d\n", show_pids, numeric_sort);
+
+    exit(EXIT_SUCCESS);
 }
