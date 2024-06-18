82b1dfb6e965a2f4880119661480d934a6037f89
zhubeilife <zhubeilife@gmail.com>
2024-06-18 16:30:20 +0800

    add lab code

---
+++ jyy-os-2024/labs/kernel_kmt/framework/main.c
@@ -0,0 +1,12 @@
+// DO NOT MODIFY: Will be reverted by the Online Judge.
+
+#include <kernel.h>
+#include <klib.h>
+
+int main() {
+    ioe_init();
+    cte_init(os->trap);
+    os->init();
+    mpe_init(os->run);
+    return 1;
+}
---
+++ jyy-os-2024/labs/kernel_kmt/include/os.h
@@ -0,0 +1,9 @@
+// When we test your kernel implementation, the framework
+// directory will be replaced. Any modifications you make
+// to its files (e.g., kernel.h) will be lost. 
+
+// Note that some code requires data structure definitions 
+// (such as `sem_t`) to compile, and these definitions are
+// not present in kernel.h. 
+
+// Include these definitions in os.h.
