e22e4acacdb5ff524f9c6b395c89a800ef203611
BryanZhang <bryan.zhang@hairobotics.com>
2024-09-18 19:17:02 +0800

    add push scripts

---
+++ libs/git_push.sh
@@ -0,0 +1,4 @@
+cd ./abstract-machine && git push origin master && cd .. && echo "Done abstract-machine" &&
+cd ./am-kernels && git push origin master && cd .. && echo "Done am-kernels" &&
+cd ./fceux-am && git push origin master && cd .. && echo "Done fceux-am" &&
+cd ./nemu && git push origin master && cd .. && echo "Done nemu"
