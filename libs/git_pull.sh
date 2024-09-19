44ed38779f5dca2aba076748e2968929b2945fbe
BryanZhang <bryan.zhang@hairobotics.com>
2024-09-19 08:48:59 +0800

    add libs update

---
+++ libs/git_pull.sh
@@ -0,0 +1,4 @@
+cd ./abstract-machine && git pull origin master && cd .. && echo "Done abstract-machine" &&
+cd ./am-kernels && git pull origin master && cd .. && echo "Done am-kernels" &&
+cd ./fceux-am && git pull origin master && cd .. && echo "Done fceux-am" &&
+cd ./nemu && git pull origin master && cd .. && echo "Done nemu"
