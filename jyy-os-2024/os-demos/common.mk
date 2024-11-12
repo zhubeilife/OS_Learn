6366b75f00acf617f024059dddc530c03855dd59
BryanZhang <bryan.zhang@hairobotics.com>
2024-11-12 20:05:36 +0800

    update

--- jyy-os-2024/os-demos/common.mk
+++ jyy-os-2024/os-demos/common.mk
@@ -1,3 +1,4 @@
 export AM_HOME := $(CURDIR)/../../../../libs/abstract-machine
 export MOSAIC := /Users/airmac/Documents/Workspace/OS_Learn/libs/mosaic
 TLIB_PATH=/Users/airmac/Documents/Workspace/OS_Learn/jyy-os-2024/os-demos/concurrency/thread-lib
+TLIB_PATH_HR=/home/kubot/Workspace/OS_Learn/jyy-os-2024/os-demos/concurrency/thread-lib
