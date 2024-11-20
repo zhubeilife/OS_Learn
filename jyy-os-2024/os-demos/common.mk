45654c4c04ecde293860177ddef8ed8863dd676f
BryanZhang <bryan.zhang@hairobotics.com>
2024-11-20 10:47:47 +0800

    update lec6

--- jyy-os-2024/os-demos/common.mk
+++ jyy-os-2024/os-demos/common.mk
@@ -1,4 +1,7 @@
+#include ../../common.mk
+#CFLAGS := -O2 -I$(TLIB_PATH) -I$(TLIB_PATH_HR) -pthread
+
 export AM_HOME := $(CURDIR)/../../../../libs/abstract-machine
-export MOSAIC := /Users/airmac/Documents/Workspace/OS_Learn/libs/mosaic
+export MOSAIC := $(CURDIR)/../../../../libs/mosaic
 TLIB_PATH=/Users/airmac/Documents/Workspace/OS_Learn/jyy-os-2024/os-demos/concurrency/thread-lib
 TLIB_PATH_HR=/home/kubot/Workspace/OS_Learn/jyy-os-2024/os-demos/concurrency/thread-lib
