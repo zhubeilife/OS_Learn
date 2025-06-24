bb1e53531a21fe1272e9b7185c8555e07e7a3ac2
zhubeilife <zhubeilife@gmail.com>
2025-06-24 18:09:51 +0800

    update new libs

--- libs/git_pull.sh
+++ libs/git_pull.sh
@@ -3,3 +3,4 @@ cd ./am-kernels && git pull origin master && cd .. && echo "Done am-kernels" &&
 cd ./fceux-am && git pull origin master && cd .. && echo "Done fceux-am" &&
 cd ./nemu && git pull origin master && cd .. && echo "Done nemu"
 cd ./nanos-lite && git pull origin master && cd .. && echo "Done nemu"
+cd ./navy-apps && git pull origin master && cd .. && echo "Done nemu"
--- libs/git_push.sh
+++ libs/git_push.sh
@@ -3,3 +3,4 @@ cd ./am-kernels && git push origin master && cd .. && echo "Done am-kernels" &&
 cd ./fceux-am && git push origin master && cd .. && echo "Done fceux-am" &&
 cd ./nemu && git push origin master && cd .. && echo "Done nemu"
 cd ./nanos-lite && git push origin master && cd .. && echo "Done nemu"
+cd ./navy-apps && git push origin master && cd .. && echo "Done nemu"
